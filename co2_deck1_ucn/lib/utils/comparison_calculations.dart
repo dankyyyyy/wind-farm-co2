33d1d78b37f7572752f3a4b7ed3fcbf3385d34c3
Max <roomax667@gmail.com>
2023-04-13 11:47:53 +0200

    declared some of the constants for calculations

---
+++ co2_deck1_ucn/lib/utils/comparison_calculations.dart
@@ -0,0 +1,8 @@
+class Calculations {
+  // values set for AO1 as of now
+  static int turbines = 27; // WTG (Wind Turbine Generator)
+  static double capacity = 9.5; // MH/WTG (MegaHenry per WTG)
+  static double expectation = 0.65; // in percent, out of maximum production
+  static double replacementLNG = 0.407; // T of CO2
+  static double replacementCoal = 0.96; // T of CO2
+}
