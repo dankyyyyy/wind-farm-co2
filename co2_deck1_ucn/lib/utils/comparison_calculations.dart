7a9e6f798699a75797377bdaceddddcdec9a6542
ppastierova <93515393+ppastierova@users.noreply.github.com>
2023-04-14 00:49:37 +0200

    Updated Calculations with a comparison CO2 chart

--- co2_deck1_ucn/lib/utils/comparison_calculations.dart
+++ co2_deck1_ucn/lib/utils/comparison_calculations.dart
@@ -8,30 +8,72 @@ class Calculations {
   double helicopters = 50; //metric tons of CO2 from the api, dummy data
   double vessels = 0; //metric tons of CO2, api, dummy data
 
+  //energy produced just by windfarm
   double calculateEnergyInDays(
       int days, int turbines, double capacity, double expectation) {
     int hours = days * 24;
-    double installedCapacity = turbines * capacity; //MW
-    double energyInDays = installedCapacity * hours * expectation;
+    double idealProduction = turbines * capacity; //MW
+    double energyInDays = idealProduction * hours * expectation;
     return energyInDays; //MWhours
   }
 
-  double co2EmittedLNG(int days) {
-    double energyInDays = calculateEnergyInDays(days, 27, 9.5, 0.65);
+  //emissions from windfarm with replacement LNG energy (35% of the time)
+  double replacementEmissionsLNG(
+      int days, int turbines, double capacity, double expectation) {
+    int hours = days * 24;
+    double replacementEmissions =
+        (turbines * capacity * hours) * ((1 - expectation) * replacementLNG);
+    return replacementEmissions;
+  }
+
+  //emissions from windfarm with replacement coal energy (35% of the time)
+  double replacementEmissionsCoal(
+      int days, int turbines, double capacity, double expectation) {
+    int hours = days * 24;
+    double replacementEmissions =
+        (turbines * capacity * hours) * ((1 - expectation) * replacementCoal);
+    return replacementEmissions;
+  }
+
+  //LNG emissions per equivalent energy produced by WindFarm
+  double co2EmittedLNG(
+      int days, int turbines, double capacity, double expectation) {
+    double energyInDays =
+        calculateEnergyInDays(days, turbines, capacity, expectation);
     double co2LNG = energyInDays * replacementLNG;
     return co2LNG; //metric tons of CO2
   }
 
-  double co2EmittedCoal(int days) {
-    double energyInDays = calculateEnergyInDays(days, 27, 9.5, 0.65);
+  double co2EmittedCoal(
+      int days, int turbines, double capacity, double expectation) {
+    double energyInDays =
+        calculateEnergyInDays(days, turbines, capacity, expectation);
     double co2COAL = energyInDays * replacementCoal;
     return co2COAL; //metric tons of CO2
   }
 
-  double co2EmittedWindFarm(double helicopters,
+  double co2EmittedTransport(double helicopters,
       double vessels) //per the same time period from the api
   {
-    double co2WF = helicopters + vessels;
-    return co2WF;
+    double transport = helicopters + vessels;
+    return transport;
+  }
+
+  double totalEmissionsCoal(int days, int turbines, double capacity,
+      double expectation, double helicopters, double vessels) {
+    double coalEmissions =
+        replacementEmissionsCoal(days, turbines, capacity, expectation);
+    double transportEmissions = co2EmittedTransport(helicopters, vessels);
+    double totalEmissionsC = coalEmissions + transportEmissions;
+    return totalEmissionsC;
+  }
+
+  double totalEmissionsLNG(int days, int turbines, double capacity,
+      double expectation, double helicopters, double vessels) {
+    double lngEmissions =
+        replacementEmissionsLNG(days, turbines, capacity, expectation);
+    double transportEmissions = co2EmittedTransport(helicopters, vessels);
+    double totalEmissions = lngEmissions + transportEmissions;
+    return totalEmissions;
   }
 }
